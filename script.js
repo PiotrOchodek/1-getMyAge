32de0b691177a670282e59a2dd690dbf9b8ae82a
Piotr Ochodek <ohis@op.pl>
2023-04-01 13:26:46 +0200

    I changed function name, delete variable, console.log and add return

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const getMyAge = input => {
-     const yourAge = Math.floor((Date.now() - Date.parse(input)) / 31556952000)
+const getAge = (input) => {
+  return Math.floor((Date.now() - Date.parse(input)) / 31556952000);
+};
 
-     return yourAge
-}
-getMyAge(1987, 11, 11)
+const myAge = getAge(1990, 1, 1);
+const myAge2 = getAge('1990');
+const myAge3 = getAge(new Date(1990, 1, 1));
+console.log(myAge);
+console.log(myAge2);
+console.log(myAge3);
